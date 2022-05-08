63d5409198da79ae6ba1f6c00682f6aa53a8eec6
Erik Cavazos <erik10cavazos@gmail.com>
2022-05-07 21:18:24 -0500

    agrego key de ironbarcode

--- Museos/Program.cs
+++ Museos/Program.cs
@@ -14,6 +14,7 @@ namespace Museos
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+            IronBarCode.License.LicenseKey = "IRONBARCODE.ERIKEVERARDO.6191-47C4EAE892-CO3YDNN2IAWJZBZ-ECZAB5UOFDM4-KGGYRXFB6WRB-73T3ZKGNX7FK-X6BZ2LIWMZP6-YCVBJH-TMPJJQTLUYCFUA-DEPLOYMENT.TRIAL-NHZSBS.TRIAL.EXPIRES.07.JUN.2022";
             Application.Run(Form1.Instance);
         }
     }
